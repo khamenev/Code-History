3d9caf3e6b1b87d740d287b7e00a82fa1995b2e9
khamenev <32979285+khamenev@users.noreply.github.com>
2023-02-15 01:38:16 +0300

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Sergey Khamenev on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
